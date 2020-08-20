d0fe5078ef7903bffb3ad51a906f0d0c26a1a321
I'm NOT Dani <61089532+NoSoyDani@users.noreply.github.com>
2020-08-20 03:44:22 +0200

    Update FormPeticion.py

--- FormPeticion.py
+++ FormPeticion.py
@@ -1,12 +1,15 @@
-
 import MacToIp as mp
 from selenium import webdriver
 
-def fPeticion(mac,msg):
+def fInit(mac):
+    global driver
     ip=mp.MacIp(mac)
     url="http://"+ip+"/"
     driver = webdriver.Chrome(r"E:\Development\ATOM WSP\chromedriver.exe")  #CHROMEDRIVER PATH
     driver.get(url)
+
+def fPeticion(msg):
+    global driver
     form = driver.find_element_by_name("txt")
     form.send_keys(msg)
     form.submit()
